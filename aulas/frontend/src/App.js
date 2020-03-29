d732f576bb852f06a82b47145729557040872975
abdhala <abdhalahrodrigues@gmail.com>
2020-03-29 17:42:31 -0300

    Finalizada a aula: 23/03 - Conhecendo a OmniStack

---
+++ aulas/frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome OmniStack.
+        </p>
+        <p>
+          May the force bey with you.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
